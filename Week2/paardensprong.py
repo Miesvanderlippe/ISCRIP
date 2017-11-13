a02da5d96bdc811b818e1dca979121ef8e05d8bb
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-11-13 21:52:28 +0100

    Move validatie is nu een functie

--- Week2/paardensprong.py
+++ Week2/paardensprong.py
@@ -8,9 +8,14 @@ def coordinates(inp: str)->tuple:
     :return: X & Y Coordinates
     """
     x = int(inp[1])
-    y = int(string.ascii_lowercase.index(inp[0]))
+    y = int(string.ascii_lowercase.index(inp[0])) + 1
 
     return x, y
+
+
+def is_valid_move(set1: tuple, set2: tuple)->bool:
+    return (abs(set1[0] - set2[0]) == 2 and abs(set1[1] - set2[1]) == 1) or \
+        (abs(set1[0] - set2[0]) == 1 and abs(set1[1] - set2[1]) == 2)
 
 
 def main()->None:
@@ -19,11 +24,10 @@ def main()->None:
     set1 = coordinates(pos1)
     set2 = coordinates(pos2)
 
-    valid = (abs(set1[0] - set2[0]) == 2 and abs(set1[1] - set2[1]) == 1) or \
-        (abs(set1[0] - set2[0]) == 1 and abs(set1[1] - set2[1]) == 2)
+    print(set1, set2)
 
     print("het paard kan {0}springen van {1} naar {2}".format(
-        ["niet ", ""][valid], pos1, pos2))
+        ["niet ", ""][is_valid_move(set1, set2)], pos1, pos2))
 
 
 if __name__ == '__main__':
