efaa1ccf85bab6d2c9459fdf623ee7342367877a
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-11-14 09:35:16 +0100

    Interessante getallen Opdracht

---
+++ Week2/interessante_getallen.py
@@ -0,0 +1,39 @@
+
+
+def totaal_cijfers(getal: int) -> int:
+    """
+    De som van de cijfers in een getal
+    :param getal: Het getal
+    :return: De som
+    """
+    return sum([int(x) for x in str(getal)])
+
+
+def int_getal(test: int) -> int:
+    """
+    Geeft het eerste getal waarvan de cijfers het getal vormen dat ook deelbaar
+    is door het getal.
+    :param test: Het getal
+    :return: Het deelbare, optelbare getal
+    """
+    num = test
+
+    while not (num % test == 0 and totaal_cijfers(num) == test):
+        num += test
+
+    return num
+
+
+def main()->None:
+    inputs = []
+    aantal_inputs = int(input("Getal tussen 0 en 50:\r\n"))
+
+    for i in range(0, aantal_inputs):
+        inputs.append(int(input("Getal tussen 0 en 100:\r\n")))
+
+    for i in inputs:
+        print(int_getal(i))
+
+
+if __name__ == '__main__':
+    main()
