358dc8139b8c7de687b55279c22080c0d9d80220
Miesvanderlippe <miesvanderlippe@gmail.com>
2017-11-21 12:51:52 +0100

    Documentatie en betere ivoer rotn opdracht

--- Week3/rotn.py
+++ Week3/rotn.py
@@ -2,6 +2,12 @@ from string import ascii_lowercase, ascii_uppercase
 
 
 def rot_shift(letter: str, shift: int) -> str:
+    """
+    Verschuift de letter n posities
+    :param letter: letter
+    :param shift: aantal posities
+    :return: verleutelde letter
+    """
     if letter in ascii_uppercase:
         return ascii_uppercase[((ascii_uppercase.index(letter) + shift) % 26)]
     elif letter in ascii_lowercase:
@@ -11,16 +17,35 @@ def rot_shift(letter: str, shift: int) -> str:
 
 
 def rotn_decode(input_str: str, shift: int) -> str:
+    """
+    Ontsleuteld een met rotn versleuteld bericht waarbij n (shift) het aantal
+    posities is.
+    :param input_str: versleuteld bericht
+    :param shift: aantal verschoven posities
+    :return: ontsleuteld bericht
+    """
     return ''.join([rot_shift(x, shift * -1) for x in input_str])
 
 
 def rotn_encode(input_str: str, shift: int) -> str:
+    """
+    Versleuteld een bericht met rotn waarbij n (shift) het aantal posities is.
+    :param input_str: Bericht
+    :param shift: Posities te verschuiven
+    :return: Verleuteld bericht
+    """
     return ''.join([rot_shift(x, shift * 1) for x in input_str])
 
 
 def main()->None:
-    print(rotn_decode("Ylluly boguhog ymn", 20))
-    print(rotn_decode(rotn_encode("Dit is een geheim", 20), 20))
+    mode = input("Encode(e) of decode (d)?\r\n").lower() == "e"
+    message = input("Bericht:\r\n")
+    shift = int(input("Shift:\r\n"))
+
+    if mode:
+        print(rotn_encode(message, shift))
+    else:
+        print(rotn_decode(message, shift))
 
 
 if __name__ == '__main__':
